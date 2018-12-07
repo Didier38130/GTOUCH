75f1c354bde8718f05be809ed679b62ce890a835
FAURE--PETRAZ Quentin <faurepeq@hostname.iut2.upmf-grenoble.fr>
2018-12-07 15:56:41 +0100

    fp

---
+++ model/data/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE compte (
+  id INTEGER,
+  login STRING,
+  mdp STRING,
+  prenom STRING,
+  nom STRING,
+  mail STRING,
+  sexe STRING,
+  telephone INTEGER,
+  adresse STRING
+);
---
+++ model/data/insert.sql
@@ -0,0 +1,2 @@
+INSERT into compte (id,login,mdp,prenom,nom,mail,sexe,telephone,adresse)
+values(1,'faurepeq','123','quentin','faure','petraz.quentin@gmail.com','homme',0750835966,'8 rue ariste berges 38000 Grenoble');
