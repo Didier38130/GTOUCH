ce3b44c2e5df8bfd869797152db35f9090b5fdb1
DYDY <DYDY@eduroam-075193.grenet.fr>
2018-12-14 14:30:54 +0100

    Modif css page d'accueil + css connexion et inscription (+petites modifs dans l'html de connexion et insciptions)

--- vue/devis.vue.php
+++ vue/devis.vue.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-<link rel="stylesheet" href="devis.vue.css">
+<link rel="stylesheet" href="css/devis.vue.css">
 <head>
   <meta charset="utf-8">
   <title>Gtouch Devis</title>
 </head>
 <body>
   <h1>Gtouch</h1>
-  <nav>
-    <a href="page_accueil.vue.php"><p>Accueil</p></a>
-    <a href="devis.vue.php"><p>Retoucher une photo</p></a>
-    <a href="mode_emploi.vue.php"><p>Mode d'emploi</p></a>
     <?php
+    include('header.view.php');
     if(isset($_SESSION['mail'])){
       echo "<a href=\"monCompte.controller.php?id=";
       echo $_SESSION['mail'];
@@ -24,7 +21,6 @@
       echo "<a href=\"../vue/connexion.vue.php\"><p>Se connecter</p></a>";
     }
     ?>
-  </nav>
   <p>Ici vous pouvez télécharger vos photos afin d'avoir l'aide d'un graphiste,
     pour ce faire nous avons besoin de quelques informations à votre sujet
     ainsi que la photo et une description des modifications que le graphiste
