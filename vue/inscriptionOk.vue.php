bce273a3cb7112d09070887e51820bf9efbb4631
ferreise <s.ferreira7@yahoo.fr>
2018-12-21 17:06:40 +0100

    Inscription réussi

--- vue/inscriptionOk.vue.php
+++ vue/inscriptionOk.vue.php
@@ -7,8 +7,9 @@
     <body>
         <?php
       include("header.view.php");
-        ?>
 
+        ?>
+        <audio autoplay="true" src="../model/inscription.mp4">
       <p>
         Votre inscritption à été confirmée, retourner à la
         <a href="../controler/page_acceuil.controler.php?id=1">Page D'acceuil</a>.
