c54866683458d528341faa30b53e40416332bc5a
DYDY <DYDY@MacBook-Pro-de-DYDY.local>
2018-12-30 19:58:01 +0100

    Merge branch 'master' of https://github.com/Didier38130/GTOUCH

--- vue/header.view.php
+++ vue/header.view.php
@@ -11,7 +11,7 @@
     <nav>
 
       <ul>
-        <li class="accueil"><a href="page_accueil.controler.php">GTOUCH</a></li>
+        <li class="accueil"><a href="page_accueil.vue.php">GTOUCH</a></li>
         <li class="annonces"><a href="#">Annonces</a></li>
         <li class="portfolios"><a href="#">Portfolios</a></li>
         <?php if (isset($_SESSION['e-mail'])) { ?>
