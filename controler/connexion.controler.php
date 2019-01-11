c810e2b002bed919255cc9af79557143ed14ed6a
DYDY <DYDY@wificampus-072146.grenet.fr>
2019-01-11 16:26:27 +0100

    Merge branch 'master' of https://github.com/Didier38130/GTOUCH

--- controler/connexion.controler.php
+++ controler/connexion.controler.php
@@ -7,33 +7,22 @@ require_once('../model/gtouchDAO.class.php');
 //////////////////////////////////////////////////////////////////////////////
 //accès à la BD
 $DAO = new gtouchDAO();
-/*
-if(!empty($_POST["mail"]) || !empty($_POST["mdp"])) {
-$mail = $_POST["mail"];
-$requete = "SELECT mdp FROM compteClient WHERE mail='$mail'";
-$q = $DAO->db()->query($requete);
-$mp = $q->fetch();
-if(password_verify($_POST["mdp"], $mp[0])) {
-session_start();
-$_SESSION["e-mail"] = $_POST["mail"];
-$_SESSION["mdp"] = $_POST["mdp"];
-header('Location: mon_compte_commandes.controler.php');
-} else {
-$message = "<label>Les informations n'ont pas permis de vous identifier</label>";
-echo 'Les informations n\'ont pas permis de vous identifier';
-}
-}
-include('../vue/connexion.vue.php');
-*/
+
+//si le email et le mdp existent et sont non vides
 if(!empty($_POST["mail"]) || !empty($_POST["mdp"])) {
   $mail = $_POST["mail"];
+  //on récupère les données du client en fonction du mail
   $q1 = $DAO->ClientExiste($mail);
+  // ou on récupère les données du graphiste en fonction du mail
   $q2 = $DAO->GraphistetExiste($mail);
+  //si c'est un client alors on récupère les données de celui corespondant à l'e-mail
   if ($q1 && !$q2) {
     $requete = "SELECT mdp FROM compteClient WHERE mail='$mail'";
     $q = $DAO->db()->query($requete);
     $mp = $q->fetch();
+    //si le mdp est bien le même que celui enregistré
     if(password_verify($_POST["mdp"], $mp[0])) {
+      //on ouvre une session
       session_start();
       $_SESSION["e-mail"] = $_POST["mail"];
       $_SESSION["mdp"] = $_POST["mdp"];
@@ -41,11 +30,14 @@ if(!empty($_POST["mail"]) || !empty($_POST["mdp"])) {
     }else{
       echo 'Les informations n\'ont pas permis de vous identifier';
     }
-  } else  if (!$q1 && $q2)  {
+  }  // si c'est un graphiste alors on récupère les données de celui corespondant à l'e-mail
+  else  if (!$q1 && $q2)  {
     $requete = "SELECT mdp FROM compteGraphiste WHERE mail='$mail'";
     $q = $DAO->db()->query($requete);
     $mp = $q->fetch();
+    //si le mdp est bien le même que celui enregistré
     if(password_verify($_POST["mdp"], $mp[0])) {
+      //on ouvre une session
       session_start();
       $_SESSION["e-mail"] = $_POST["mail"];
       $_SESSION["mdp"] = $_POST["mdp"];
@@ -53,7 +45,8 @@ if(!empty($_POST["mail"]) || !empty($_POST["mdp"])) {
     }else{
       echo 'Les informations n\'ont pas permis de vous identifier';
     }
-  } else {
+  }//sinon l'utilisateur n'est pas encore enregistré sur la plateforme
+   else {
     echo 'Votre login est inconnu.Veuillez vous inscrire';
   }
 }
